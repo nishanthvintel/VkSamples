7317c8f6f1cecdd83fb29932f68c2ecd90715fa3
Nishanth V <nishanth.v@intel.com>
2022-09-22 22:42:57 +0530

    [Clear] Create Physical Device

--- src/Clear/source/main.cpp
+++ src/Clear/source/main.cpp
@@ -8,6 +8,8 @@
 
 #include <iostream>
 #include <Windows.h>
+#include <vector>
+#include <optional>
 
 class ClearApp
 {
@@ -22,32 +24,71 @@ public:
 
 private:
     VkInstance instance;
+    VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
     GLFWwindow* window;
     
     const int WIDTH  = 800;
     const int HEIGHT = 600;
+
+    const std::vector<const char*> validationLayers = {
+        "VK_LAYER_KHRONOS_validation"
+    };
+
+#ifdef NDEBUG
+    const bool enableValidationLayers = false;
+#else
+    const bool enableValidationLayers = true;
+#endif
+
+    struct QueueFamilyIndices
+    {
+        std::optional<uint32_t> graphicsFamily;
+
+        bool isComplete()
+        {
+            return graphicsFamily.has_value();
+        }
+    };
 
     void initWindow()
     {
         glfwInit();
         glfwWindowHint( GLFW_CLIENT_API, GLFW_NO_API );
 
-        window = glfwCreateWindow( WIDTH, HEIGHT, "Course01", nullptr, nullptr );
-
-        uint32_t extensionCount = 0;
-
-        vkEnumerateInstanceExtensionProperties( nullptr, &extensionCount, nullptr );
-
-        printf( "Vulkan extension count: %i\n", extensionCount );
-
-        glm::mat4x4 testMatrix( 1.0f );
-        glm::vec4 testVector( 1.0f );
-
-        auto resultMul = testMatrix * testVector;
-
-
+        window = glfwCreateWindow( WIDTH, HEIGHT, "Clear Application", nullptr, nullptr );
     }
 
+    bool checkValidationLayerSupport()
+    {
+        uint32_t layerCount = 0;
+
+        vkEnumerateInstanceLayerProperties( &layerCount, nullptr );
+
+        std::vector<VkLayerProperties> layers( layerCount );
+
+        vkEnumerateInstanceLayerProperties( &layerCount, layers.data() );
+
+        for( const char* layerName : validationLayers )
+        {
+            bool layerFound = false;
+
+            for( const auto& layerProperties : layers )
+            {
+                if( !strcmp( layerName, layerProperties.layerName ) )
+                {
+                    layerFound = true;
+                    break;
+                }
+
+            }
+            if( !layerFound )
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
     void createInstance()
     {
         uint32_t glfwExtnCnt = 0;
@@ -61,19 +102,117 @@ private:
         appInfo.engineVersion = VK_MAKE_VERSION( 1, 0, 0 );
         appInfo.apiVersion = VK_API_VERSION_1_0;
 
-        VkInstanceCreateInfo instanceCreateInfo = {};
-        instanceCreateInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
-        instanceCreateInfo.pApplicationInfo = &appInfo;
-        instanceCreateInfo.enabledExtensionCount = glfwExtnCnt;
-        instanceCreateInfo.ppEnabledExtensionNames = glfwExtns;
-        instanceCreateInfo.enabledLayerCount = 0;
+        VkInstanceCreateInfo createInfo = {};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+        createInfo.enabledExtensionCount = glfwExtnCnt;
+        createInfo.ppEnabledExtensionNames = glfwExtns;
 
-        VkResult result = vkCreateInstance( &instanceCreateInfo, nullptr, &instance );
+        if( enableValidationLayers )
+        {
+            if( !checkValidationLayerSupport() )
+            {
+                throw std::runtime_error( "Validation Layer requested but not supported" );
+            }
+
+            createInfo.enabledLayerCount = static_cast<uint32_t>( validationLayers.size() );
+            createInfo.ppEnabledLayerNames = validationLayers.data();
+        }
+        else
+        {
+            createInfo.enabledLayerCount = 0;
+        }
+
+        VkResult result = vkCreateInstance( &createInfo, nullptr, &instance );
+        if( result != VK_SUCCESS )
+        {
+            throw std::runtime_error( "Failed to create Instance!" );
+        }
+
+        uint32_t extensionCount = 0;
+        vkEnumerateInstanceExtensionProperties( nullptr, &extensionCount, nullptr );
+
+        std::vector<VkExtensionProperties> extensions( extensionCount );
+        vkEnumerateInstanceExtensionProperties( nullptr, &extensionCount, extensions.data() );
+
+        std::cout << "Available " << extensionCount << " extensions:" << std::endl;
+        for( const auto extension : extensions )
+        {
+            std::cout << extension.extensionName << std::endl;
+        }
+    }
+
+    QueueFamilyIndices findQueueFamilies( VkPhysicalDevice device )
+    {
+        QueueFamilyIndices indices;
+        uint32_t queueFamilyCount = 0;
+
+        vkGetPhysicalDeviceQueueFamilyProperties( device, &queueFamilyCount, nullptr );
+
+        std::vector<VkQueueFamilyProperties> queueFamilies( queueFamilyCount );
+        vkGetPhysicalDeviceQueueFamilyProperties( device, &queueFamilyCount, queueFamilies.data() );
+
+        int i = 0;
+
+        for( const auto& queueFamily : queueFamilies )
+        {
+            if( queueFamily.queueFlags & VK_QUEUE_GRAPHICS_BIT )
+            {
+                indices.graphicsFamily = i;
+                break;
+            }
+
+            i++;
+        }
+
+        return indices;
+    }
+
+    bool isDeviceSuitable( VkPhysicalDevice device )
+    {
+#if 0
+        VkPhysicalDeviceFeatures features;
+        VkPhysicalDeviceProperties properties;
+        vkGetPhysicalDeviceFeatures( device, &features );
+        vkGetPhysicalDeviceProperties( device, &properties );
+
+        return ( properties.deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU && features.geometryShader );
+#else
+        return findQueueFamilies( device ).isComplete();
+#endif
+    }
+
+    void pickPhysicalDevice()
+    {
+        uint32_t deviceCount = 0;
+        vkEnumeratePhysicalDevices( instance, &deviceCount, nullptr );
+
+        if( deviceCount == 0 )
+        {
+            throw std::runtime_error( "Could not find device with Vulkan support!" );
+        }
+
+        std::vector<VkPhysicalDevice> devices( deviceCount );
+        vkEnumeratePhysicalDevices( instance, &deviceCount, devices.data() );
+
+        for( const auto& device : devices )
+        {
+            if( isDeviceSuitable( device ) )
+            {
+                physicalDevice = device;
+            }
+        }
+
+        if( physicalDevice == VK_NULL_HANDLE )
+        {
+            throw std::runtime_error( "Could not find compatible device!" );
+        }
     }
 
     void initVulkan()
     {
         createInstance();
+        pickPhysicalDevice();
     }
 
     void mainloop()
@@ -94,7 +233,8 @@ private:
     }
 };
 
-int CALLBACK WinMain( _In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd )
+//int CALLBACK WinMain( _In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd )
+int main()
 {
     ClearApp app;
     try
@@ -109,4 +249,3 @@ int CALLBACK WinMain( _In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance
 
     return EXIT_SUCCESS;
 }
-
