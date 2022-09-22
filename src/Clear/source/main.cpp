69998f55608bee5833caeff0183b41ef8aec1c6e
Nishanth V <nishanth.v@intel.com>
2022-09-22 23:38:02 +0530

    [Clear] Create Logical Device no present

--- src/Clear/source/main.cpp
+++ src/Clear/source/main.cpp
@@ -25,6 +25,8 @@ public:
 private:
     VkInstance instance;
     VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
+    VkDevice device;
+
     GLFWwindow* window;
     
     const int WIDTH  = 800;
@@ -200,6 +202,9 @@ private:
             if( isDeviceSuitable( device ) )
             {
                 physicalDevice = device;
+
+                // stop and use first available physical device
+                break;
             }
         }
 
@@ -209,10 +214,48 @@ private:
         }
     }
 
+    void createLogicalDevice()
+    {
+        QueueFamilyIndices indices = findQueueFamilies( physicalDevice );
+
+        VkDeviceQueueCreateInfo queueCreateInfo{};
+        queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+        queueCreateInfo.queueFamilyIndex = indices.graphicsFamily.value();
+        queueCreateInfo.queueCount = 1;
+
+        float queuePriority = 1.0f;
+        queueCreateInfo.pQueuePriorities = &queuePriority;
+
+        VkPhysicalDeviceFeatures deviceFeatures{};
+
+        VkDeviceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+        createInfo.pQueueCreateInfos = &queueCreateInfo;
+        createInfo.queueCreateInfoCount = 1;
+        createInfo.pEnabledFeatures = &deviceFeatures;
+        createInfo.enabledExtensionCount = 0;
+
+        if( enableValidationLayers )
+        {
+            createInfo.enabledLayerCount = static_cast<uint32_t>( validationLayers.size() );
+            createInfo.ppEnabledLayerNames = validationLayers.data();
+        }
+        else
+        {
+            createInfo.enabledLayerCount = 0;
+        }
+
+        if( vkCreateDevice( physicalDevice, &createInfo, nullptr, &device ) != VK_SUCCESS )
+        {
+            throw std::runtime_error( "Could not create Vulkan Logical Device!" );
+        }
+    }
+
     void initVulkan()
     {
         createInstance();
         pickPhysicalDevice();
+        createLogicalDevice();
     }
 
     void mainloop()
@@ -225,6 +268,7 @@ private:
 
     void cleanup()
     {
+        vkDestroyDevice( device, nullptr );
         vkDestroyInstance( instance, nullptr );
 
         glfwDestroyWindow( window );
